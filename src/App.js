eb72dad1102ba44af0661d8d72f63e0482f7b8c3
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-02-25 15:02:33 +0100

    fixat grunden till vårt arbete!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import "./Css/nav.css";
 
@@ -7,29 +8,10 @@ import Main from "./Components/Main";
 
 function App() {
   return (
-    <div className="App">
-        <div className="sideLeft">
-          <div>
-            Logo
-          </div>
-          <ul>
-            <li> Link 1 </li>
-            <li> Link 7 </li>
-            <li> Link 3 </li>
-            <li> Link 1 </li>
-          </ul>
-        </div>
-
-        <div className={"bigBox"}>
-          <header>
-            <input placeholder="Search" type="text" />
-            <button>Log out</button>
-          </header>
-
-          <main>
-            <Main />
-          </main>
-        </div>
+    <div clasclassName="App">
+      <Router>
+        <Route path="/main" component={Main} />
+      </Router>
     </div>
   );
 }
