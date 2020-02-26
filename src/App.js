fa67928419b73bf2c4c1dcd430fdf4fbb6528bc2
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-02-26 14:12:52 +0100

    setting up , token done!

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import "./Css/nav.css";
 import Main from "./Components/Main";
 import LogIn from "./Components/LogIn";
 import GetToken from "./Components/GetToken";
+import DropBoxFolder from "./Components/DropBoxFolder";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Route path="/main" component={Main} /> */}
-        <Route path="/login" component={LogIn} />
+        <Route exact path="/login" component={LogIn} />
         <Route path="/auth" component={GetToken} />
+        <Route path="/main" component={Main} />
+        <Route path="/edit-folder" component={DropBoxFolder} />
       </Router>
     </div>
   );
