52fbbe1d2e98d523d52c53133101c17cc9afe42d
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-02-27 14:24:49 +0100

    auto redirect to main if token is available

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/" component={LogIn} />
         <Route path="/auth" component={GetToken} />
 
         <Route path="/main" component={Main} />
