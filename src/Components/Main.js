76760c12d2b5a091e888beda56ab4cf88b7e8e9b
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-02-28 02:07:17 +0100

    folder thumbnail funkar, men inte file!

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,12 +7,17 @@ import LogOut from './LogOut';
 
 import '../Css/nav.css'
 
+import folderImg from '../Img/folder-img.png';
+import fileImg from '../Img/file-img.png';
+
 class Main extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-          folders: []
+          folders: [],
+          thumbnail: {},
+          files: []
         }
     }
 
@@ -20,12 +25,75 @@ class Main extends Component {
       const dbx = new Dropbox({ accessToken: localStorage.getItem("token") });
       dbx.filesListFolder({ path: "" })
         .then((res) => {
+          console.log(res.entries);
           this.setState({ folders: res.entries });
+
+          // get thumbnails for the current batch of files
+      // dbx.filesGetThumbnailBatch({ 
+      //   entries: res.entries.map(function(entry){
+      //     console.log(entry);
+      //       return {
+      //         path: entry.id,
+      //         format : {'.tag': 'jpeg'},
+      //         size: { '.tag': 'w2048h1536'},
+      //         mode: { '.tag': 'strict' }
+      //       }
+      //   })
+      // });
         });
+
+
+        // get images from image files!
+        // dbx.filesGetThumbnailBatch({
+        //   entries: [{
+        //     path: '/image1.png',
+        //     size: 'w32h32',
+        //     format: 'png'
+        //   }]
+        // })
+        // .then(res => {
+        //   console.log(res);
+        // })
+
+        // dbx.filesGetThumbnailBatch({
+        //   entries: 
+        // })
+        // .then(res => {
+        //   console.log(res);
+        // })
+
+        // dbx.filesGetThumbnail({"path": "/image1.png"})
+        // .then(res => {
+        //   // console.log(res);
+        //   this.setState({ thumbnail: res });
+        // })
+    }
+
+    componentDidUpdate() {
+      const { folders, files } = this.state;
+      folders.map(Files => {
+        const type = Files['.tag'];
+
+        if (type === 'file') {
+          // console.log(Files.name);
+        }
+      });
+
+      // console.log(files);
+
+      const dbx = new Dropbox({ accessToken: localStorage.getItem("token") });
+      dbx.filesGetThumbnailBatch({  
+        entries: [{  
+          path: '/image1.png image-142881.jpg',  
+          size: 'w32h32',  
+          format: 'png',  
+        }]  
+      })
+      // .then(res => console.log(res))
     }
 
     render() {
-      const { folders } = this.state;
+      const { folders, thumbnail } = this.state;
 
         return (
           <div className="App">
@@ -44,7 +112,6 @@ class Main extends Component {
         <div className={"bigBox"}>
           <header>
             <input placeholder="Search" type="text" />
-            {/* <button>Log out</button> */}
             <LogOut />
           </header>
 
@@ -58,12 +125,67 @@ class Main extends Component {
                   </thead>
 
                   <tbody>
+                    {folders.filter(f => f['.tag'] === 'file').map(fl =>{
+                      // console.log(fl);
+                    })
+                    }
+
+
                     {folders.map(folder => {
+                      // render img icon to files and folders!
+                      const type = folder['.tag'];
+                      let fileThumbnail
+                      let folderThumbnail
+                      // type === 'folder' ? folderThumbnail = folderImg : folderThumbnail = fileImg;
+
+                      if (type === 'folder') {
+                        folderThumbnail = folderImg;
+                      }
+                      else {
+                        // folderThumbnail = fileImg;
+                        // console.log(folder.path_display);
+
+                        let img = folder.path_display;
+
+                        console.log(img);
+
+                        const dbx = new Dropbox({ accessToken: localStorage.getItem("token") });
+
+                        //get images from image files!
+                        dbx.filesGetThumbnailBatch({
+                          entries: [{
+                            path: `${img}`,
+                            size: 'w32h32',
+                            format: 'png'
+                          }]
+                        })
+                        .then(res => {
+                          console.log(res.entries[0].thumbnail);
+                          // folderThumbnail = res.entries[0].thumbnail;
+                          folderThumbnail = `data:image/jpeg;base64, ${res.entries[0].thumbnail}`;
+                          //"data:image/jpeg;base64, ${file.thumbnail}"
+                          // fileThumbnail = res.
+                        })
+
+                        // let obj = {};
+                        // obj = folder
+
+                        // let arr = [];
+                        // let arr2 = [];
+
+                        // arr.push(obj);
+                        // arr2.push(arr)
+
+                        // console.log(arr2);
+                      }
+
+                      // let objectUrl = URL.createObjectURL(thumbnail.fileBlob);
                       return (
                         <tr>
-                          <div>
-                            <Link to={`/folder${folder.path_display}`}>
-                              <td>{folder.name}</td>
+                          <div style={{ display: 'flex' }}>
+                              <img src={folderThumbnail} style={{ height: '42px', width: '42px' }} alt=""/>
+                            <Link to={`/folder${folder.path_display}`} style={{ border: '1px solid' }}>
+                                <td>{folder.name}</td>
                             </Link>
                           </div>
                         </tr>
