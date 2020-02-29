c2e7b540789979683c350ec67590a0b84c2a373f
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-02-29 23:58:24 +0100

    klar med bilderna!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./Css/nav.css";
 import Main from "./Components/Main";
 import LogIn from "./Components/LogIn";
 import GetToken from "./Components/GetToken";
-import DropBoxFolder from "./Components/DropBoxFolder";
+// import DropBoxFolder from "./Components/DropBoxFolder";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/auth" component={GetToken} />
 
         <Route path="/main" component={Main} />
-        <Route path="/folder/:path" render={(props) => <DropBoxFolder id={props.match.params.path} />} />
+        {/* <Route path="/folder/:path" render={(props) => <DropBoxFolder id={props.match.params.path} />} /> */}
       </Router>
     </div>
   );
