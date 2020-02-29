0c713004bc9a3ba973d888f47219501fd8989124
Chau Kha Doan <kha.doanchau@gmail.com>
2020-02-29 17:23:07 +0100

    Delete, dropmenu and warning window added

---
+++ src/Components/ClickedOutsideRemover.js
@@ -0,0 +1,40 @@
+import React, { Component, PureComponent } from 'react'
+import '../Css/Options.css'
+
+class ClickedOutsideRemover extends PureComponent{
+  constructor(props){
+    super(props)
+    this.wrapper = React.createRef()
+    this.state = {
+      show: false
+    }
+
+  }
+  componentDidMount(){
+    document.addEventListener('click', this.HandleOnClickOutsideOfElement)
+  }
+  componentWillUnmount(){
+    document.addEventListener('click', this.HandleOnClickOutsideOfElement)
+  }
+  HandleOnClickOutsideOfElement = (e) =>{
+
+    if(this.wrapper.current === e.target){
+      this.setState({show: !this.state.show})
+    }else{
+      this.setState({show: false})
+    }
+  }
+
+  render(){
+    const{show} = this.state
+  return(
+    <>
+     <div className="dropMenu" ref={this.wrapper}>{show ? this.props.children : null}</div>
+    </>
+
+  )}
+
+}
+
+
+export default ClickedOutsideRemover
