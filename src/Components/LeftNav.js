43241b6d538ae7bac3002929c1c937246df0e7e4
Martynas Majaras <martynasmajaras98@gmail.com>
2020-03-09 13:04:26 +0100

    changes

--- src/Components/LeftNav.js
+++ src/Components/LeftNav.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
-
+import Logo from '../LogoDropbox.png'
 import '../Css/icons.css'
 import '../Css/mainFiles.css'
 import '../Css/nav.css'
 import '../Css/UlItems.css'
 
-
  class LeftNav extends Component {
     render() {
         return (
                 <div className="sideLeft">
-                <div className="Logo">
-                    Logo
-                </div>
+                 <img src={Logo} />
                 <ul>
                     <li> Start </li>
                     <br/>
