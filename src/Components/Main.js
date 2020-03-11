a313c4561b7dbb67a42572d1aa65f070caa9c526
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-03-11 15:44:11 +0100

    g

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -37,7 +37,7 @@ class Main extends Component {
 
 
     copy = (original_path, your_path) =>{
-     
+    
       this.dbx.filesCopy({
         from_path: original_path,
         to_path: your_path,
@@ -122,9 +122,9 @@ class Main extends Component {
   }
 
   search_FOLDERS_FILES = (e) => {
-    let search
     
-    this.dbx.filesSearch({ path: '' , query: "/"})
+    
+    this.dbx.filesSearch({ path: '' , query: e.target.value})
     .then(res => {
       console.log(res)
       let entries = res.matches.map(x => x.metadata);
