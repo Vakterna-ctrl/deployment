206654f5da5c07a1835c5c950e715c9aa3c15e22
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-11 11:00:32 +0100

    testing fungerar

---
+++ src/Components/Testning.test.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { expect } from 'chai';
+import { shallow, configure} from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import RightNav from './RightNav';
+import Header from './Header';
+
+configure ({adapter: new Adapter()})
+
+describe('<RightNav />', () => {
+    it('renders four <li> Elements', () => {
+        const wrapper = shallow(<RightNav />);
+        expect(wrapper.find('li')).to.have.lengthOf(4);
+    });
+});
+
+describe('<Header />', () => {
+    it('simulates onChange events on <input> Element', () => {
+    const onChange = sinon.spy();
+    const wrapper = shallow(<Header onChange={onChange} />);
+    wrapper.find('input').simulate('change', { target: {
+        value: 'Change function' }
+    });
+});});
